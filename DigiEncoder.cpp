b0a47d4fa009962c52a0a2dfb2f664f7111ac9f2
Alexandru Nasalciuc <196249310+alexnasa-pers@users.noreply.github.com>
2026-03-17 19:06:33 +0000

    Had to move the forward funtion to main so it could send messages to client easily

--- DigiEncoder.cpp
+++ DigiEncoder.cpp
@@ -1,21 +1,32 @@
 #include "DigiEncoder.h"
 #include <Arduino.h>
-#include "Driver.h"
+#include "driver.h"
 
 DigiEncoder::DigiEncoder(int pin, L293D &drv)
   : Pin(pin), Count(0), GoalCount(0), traveldist(0.0f), driver(drv) {}
 
 void DigiEncoder::EncBegin() {
-  pinMode(Pin, INPUT);
+  pinMode(Pin, INPUT_PULLUP);
 }
 
 void DigiEncoder::Increase() {
   Count++;
 }
 
-void DigiEncoder::StopAt(float traveldist) {
-  GoalCount = (CIRCUMFERENCE / traveldist) * 8; // 8 pulses per rev
-  if (Count >= GoalCount) {
-    driver.setspeed(0);
-  }
+void DigiEncoder::RightTurn(){
+  driver.setLspeed(180);
+  driver.setRspeed(-180);
+  delay(rightTurn90Time);
+  driver.stopBuggy();
+}
+
+void DigiEncoder::LeftTurn(){
+  driver.setLspeed(-180);
+  driver.setRspeed(180);
+  delay(leftTurn90Time);
+  driver.stopBuggy();
+}
+
+float DigiEncoder::DistanceTravelled(int Count) {
+  return ((float(Count)/8)*20.4);
 }
